34cf10ac8e253e7cd3a6bc78f8819fd47bbdd324
blobbo7 <84816363+blobbo7@users.noreply.github.com>
2026-07-25 17:45:51 -0600

    Green

---
+++ green.py
@@ -0,0 +1,16 @@
+import os, random
+randwords = ["Tree", "Green", "Purple", "Walter", "Emo"]
+os.system('cat thisiswhatchanges.txt')
+
+file1 = open("thisiswhatchanges.txt", "w") # change file
+random.shuffle(randwords)
+file1.writelines(randwords)
+file1.close()
+
+os.system('git add .')
+os.system(f'git commit -m "{random.choice(randwords)}"')
+os.system('git push')
+
+# git add .
+# git commit -m "random name i guess"
+# git push
